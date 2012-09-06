b89c21bdde0303fac40844c86fcd986675d41878
Benjamin J. Balter <ben@balter.com>
2012-09-06 17:39:59 -0400

    cleanup config

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$args = array( 
-	'name' => 'wordpress',
-	'port' => '8889',
-	'table_blacklist' => array( 'wp_options' ),
-);
-
-register_db_api( 'test-db', $args );
---
+++ config.sample.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Example configuration... all fields optional except dataset name
+
+$args = array( 
+            'name' => null,
+            'username' => 'root',
+            'password' => 'root',
+            'server' => 'localhost',
+            'port' => 3306,
+            'type' => 'mysql',
+            'table_blacklist' => array(),
+            'column_blacklist' => array(),
+);
+
+register_db_api( 'dataset-name', $args );
+
+*/
